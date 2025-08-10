471046532d38f2259b095ec0ae0138cf214d13f0
gesrod2695 <gesrod2695@icloud.com>
2025-08-09 22:18:59 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,19 +1,33 @@
-# Fixed version for your server/djangoapp/models.py file
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CarMake(models.Model):
-    name = models.CharField(max_length=100, help_text='Enter car make name (e.g. BMW, Toyota)')
-    description = models.TextField(max_length=1000, help_text='Enter a brief description of the car make')
-    
+    name = models.CharField(
+        max_length=100,
+        help_text='Enter car make name (e.g. BMW, Toyota)'
+    )
+    description = models.TextField(
+        max_length=1000,
+        help_text='Enter a brief description of the car make'
+    )
+
     # Additional fields
-    founded_year = models.IntegerField(null=True, blank=True, help_text='Year the company was founded')
-    headquarters = models.CharField(max_length=100, null=True, blank=True, help_text='Company headquarters location')
-    
+    founded_year = models.IntegerField(
+        null=True,
+        blank=True,
+        help_text='Year the company was founded'
+    )
+    headquarters = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        help_text='Company headquarters location'
+    )
+
     def __str__(self):
         return self.name  # Return the name when printed
+
 
 class CarModel(models.Model):
     # Type choices for the car model
@@ -24,7 +38,7 @@ class CarModel(models.Model):
     COUPE = 'Coupe'
     CONVERTIBLE = 'Convertible'
     TRUCK = 'Truck'
-    
+
     CAR_TYPES = [
         (SEDAN, 'Sedan'),
         (SUV, 'SUV'),
@@ -34,16 +48,19 @@ class CarModel(models.Model):
         (CONVERTIBLE, 'Convertible'),
         (TRUCK, 'Truck'),
     ]
-    
+
     # Many-To-One relationship to Car Make model
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    
+
     # Dealer ID (refers to dealer in external database)
     dealer_id = models.IntegerField()
-    
+
     # Name of the car model
-    name = models.CharField(max_length=100, help_text='Enter car model name (e.g. Camry, Accord)')
-    
+    name = models.CharField(
+        max_length=100,
+        help_text='Enter car model name (e.g. Camry, Accord)'
+    )
+
     # Type with limited choices
     type = models.CharField(
         max_length=20,
@@ -51,7 +68,7 @@ class CarModel(models.Model):
         default=SEDAN,
         help_text='Select car type'
     )
-    
+
     # Year as IntegerField (not DateField as the lab requires)
     year = models.IntegerField(
         default=2023,
@@ -61,11 +78,22 @@ class CarModel(models.Model):
         ],
         help_text='Enter year (2015-2023)'
     )
-    
+
     # Additional fields
-    color = models.CharField(max_length=50, null=True, blank=True, help_text='Car color')
+    color = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        help_text='Car color'
+    )
     mileage = models.IntegerField(default=0, help_text='Car mileage')
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, help_text='Car price')
-    
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        help_text='Car price'
+    )
+
     def __str__(self):
-        return f"{self.car_make.name} {self.name} ({self.year})"  # Return car make and model when printed
+        return f"{self.car_make.name} {self.name} ({self.year})"
